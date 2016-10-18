d0c8f33581ec7862064cded0a6ecbe0738f6e3cc
Diego Said Anaya Mancilla <anayamd96@gmail.com>
2016-10-18 07:40:32 -0500

    Solved 2 CF problems in competition

---
+++ Codeforces/Round377/B.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <sstream>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <cctype>
+#include <string>
+#include <vector>
+#include <list>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <functional>
+using namespace std; 
+#define DEBUG(x) cout << '>' << #x << ':' << x << endl;
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define FORD(i,a,b) for(int i=(a);i>=(b);i--)
+#define spresent(str1,str2) (str1.find(str2) != string::npos)
+inline bool EQ(double a, double b) { return fabs(a-b) < 1e-9; }
+const int INF = 1<<29;
+typedef long long ll;
+inline int two(int n) { return 1 << n; }
+inline int test(int n, int b) { return (n>>b)&1; }
+inline void set_bit(int & n, int b) { n |= two(b); }
+inline void unset_bit(int & n, int b) { n &= ~two(b); }
+inline int last_bit(int n) { return n & (-n); }
+inline int ones(int n) { int res = 0; while(n && ++res) n-=n&(-n); return res; }
+template<class T> void chmax(T & a, const T & b) { a = max(a, b); }
+template<class T> void chmin(T & a, const T & b) { a = min(a, b); }
+/////////////////////////////////////////////////////////////////////
+int main() {
+    int n, k;
+    while(cin>>n>>k) {
+        vector<int> v(n+1);
+        REP(i,n) cin>>v[i];
+
+        vector<int> b = v;
+        for(int i = 1; i < n; i++) {
+            if(b[i] + b[i-1] >= k) continue;
+            b[i] += k - (b[i] + b[i-1]);
+        }
+
+        long long cnt = 0;
+        REP(i,n) cnt += max(b[i] - v[i], 0);
+
+        cout<<cnt<<endl;
+        REP(i,n-1) cout<<b[i]<<" ";
+        cout<<b[n-1]<<endl;
+    }
+
+    return 0;
+}
