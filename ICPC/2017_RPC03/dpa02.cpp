19bc1cdc30ef5c225f8171c54cc5954fd60e06b0
Diego Said Anaya Mancilla <mancilladev@gmail.com>
2017-04-03 20:30:30 -0500

    Solve 1 ICPC problem

--- ICPC/2017_RPC03/dpa02.cpp
+++ ICPC/2017_RPC03/dpa02.cpp
@@ -2,29 +2,51 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+typedef long long ll;
 using namespace std;
 
+int LeastPower(ll a, ll x) {
+    // Least power (a^k) that does not divide x
+    int b = a, k = 0;
+    while (x % a == 0)
+        a *= b, ++k;
+    return k+1;
+}
+
 int main() {
+    vector<int> isprime(10000, 1), primes;
+    for (int i = 2; i < isprime.size(); ++i) if (isprime[i]) {
+        primes.push_back(i);
+        for (int j = i*i; j < isprime.size(); j += i) isprime[j] = 0;
+    }
+
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
     int t; cin >> t;
     
     while (t--) {
-        unsigned long long n; cin >> n;
-        unsigned long long sum = 1;
+        ll cur; cin >> cur;
+        ll n = cur;
+        ll sum = 1;
 
-        for (long long i = 2; i <= sqrt(n); ++i) {
-            if (n%i == 0) {
-                if (i == n/i)
-                    sum += i;
-                else
-                    sum += i + n/i;
-            }
+        int i = 0;
+        for (auto prime: primes) {
+            if (n <= 1) break;
+            // Sum factors 120 = (1+2+2^2+2^3)*(1+3)*(1+5) = 15*4*6 = 360
+            // p^0 + p^1 + ... + p^(h-1) = (p^h-1) / (p-1)
+
+            int h = LeastPower(prime, n);
+            ll ph = pow(prime, h);
+            ll summationpi = (ph - 1) / (prime - 1);
+            sum *= summationpi;
+            n /= ph/prime;
         }
 
-        if (sum > n) cout << "abundant\n";
-        else if (sum < n) cout << "deficient\n";
+        sum -= cur;
+
+        if (sum > cur) cout << "abundant\n";
+        else if (sum < cur) cout << "deficient\n";
         else cout << "perfect\n";
     }
 
---
+++ ICPC/2017_RPC03/mtestgen.py
@@ -0,0 +1,6 @@
+from random import randint
+
+print(1100)
+for i in range(1100):
+    print(randint(2, 10**12))
+
