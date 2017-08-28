adc81836a8827eb2753dde48c0c1eeb03416bdf7
Diego Said Anaya Mancilla <redcpp@gmail.com>
2017-08-28 11:52:50 -0500

    Change cpp template

--- templates/cpp_template.cpp
+++ templates/cpp_template.cpp
@@ -1,17 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define PB push_back
-typedef long long ll;
-const ll INF = ll(1e18);
-
-#ifndef ONLINE_JUDGE
-    freopen("in", "r", stdin);
-#else
-    ios_base::sync_with_stdio(0);
-    cin.tie(nullptr);
-#endif
+typedef long double ld;
+const ld INF = ld(1e18);
 
 int main(void) {
+    ios_base::sync_with_stdio(0), cin.tie(nullptr);
 
     return 0;
 }
