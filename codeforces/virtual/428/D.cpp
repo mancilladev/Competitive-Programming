0d301dc80ea6a6ffe5868e63081edd7ebd835daa
Diego Said Anaya Mancilla <redcpp@gmail.com>
2017-08-31 13:07:45 -0500

    Small practice on CF

--- codeforces/virtual/428/D.cpp
+++ codeforces/virtual/428/D.cpp
@@ -4,54 +4,56 @@ using namespace std;
 typedef long long ll;
 typedef long double ld;
 const ll INF = ll(1e18);
+const ll MOD = (ll)1e9 + 7;
 
-const int MOD = (int)1e9 + 7;
-const int N = 1010100;
-ll a[N], cnt[N], dp[N], power[N];
-ll ans = 0, maxi = 0;
-ll n;
+ll sub(ll x, ll y) {
+    x -= y;
+    if (x < 0)
+        x += MOD;
+    return x;
+}
+
+ll add(ll x, ll y) {
+    return (x + y) % MOD;
+}
+
+ll mult(ll x, ll y) {
+    return (x * y) % MOD;
+}
+
+const int N = (int)1e6 + 7;
+int cnt[N] {0};
+ll power[N];
+ll a[N];
 
 int main(void) {
     // freopen("in", "r", stdin);
     // freopen("out", "w", stdout);
     ios_base::sync_with_stdio(0), cin.tie(nullptr);
-    cin >> n;
 
-    // powers
     power[0] = 1;
-    for (ll i = 1; i < N; ++i)
-        power[i] = (power[i-1] * 2) % MOD;
+    for (int i = 1; i < N; ++i)
+        power[i] = mult(power[i - 1], 2);
 
-    // common divisors
-    for (ll i = 0; i < n; ++i) {
-        cin >> a[i];
-        maxi = max(maxi, a[i]);
-        for (ll j = 2; j * j <= a[i]; ++j) if (a[i]%j == 0) {
-            cnt[j]++;
-            if (a[i]/j != j)
-                cnt[a[i]/j]++;
-        }
-        cnt[a[i]]++;
+    int n, x;
+    cin >> n;
+    while (n--)
+        cin >> x, cnt[x]++;
+    for (int i = 1; i < N; ++i) {
+        int y = 0;
+        for (int j = i; j < N; j += i)
+            y += cnt[j];
+        if (y == 0) continue;
+        a[i] = mult(y, power[y - 1]);
     }
 
-    // step 2
-    for (int i = maxi; i >= 2; --i) {
-        ll minus = 0;
-        if (cnt[i] > 0) {
-            if (i <= maxi/2)
-                for (ll j = i * 2; j > 0 && j <= maxi; j += i)
-                    minus += dp[i];
-            dp[i] = (cnt[i] * power[cnt[i] - 1]) % MOD;
-            dp[i] -= minus;
-            dp[i] = (dp[i]%MOD + MOD) % MOD;
-            ans = (ans + (dp[i] * i)%MOD) % MOD;
-        } else {
-            dp[i] = 0;
-        }
+    ll ans = 0;
+    for (int x = N - 1; x > 1; --x) {
+        for (int y = 2 * x; y < N; y += x)
+            a[x] = sub(a[x], a[y]);
+        ans = add(ans, mult(x, a[x]));
     }
-
-    cout << ans << endl;
+    printf("%lld\n", ans);
 
     return 0;
 }
-
