2143fd5d6714019e01cfd949c38dabbd95f311ed
Diego Said Anaya Mancilla <redcpp@gmail.com>
2017-10-16 21:44:36 -0500

    Better templates for c++

--- templates/codejam.cpp
+++ templates/codejam.cpp
@@ -1,11 +1,23 @@
 #include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
 using namespace std;
-#define error(x) cerr << #x << " is " << x << endl
-#define mt make_tuple
+using namespace __gnu_pbds;
+ 
+typedef long long ll;
+template <class T> using Tree = tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+
+#define error(x) cerr << #x << " = " << x << endl
+#define sz(x) (int)(x).size()
 #define eb emplace_back
 #define pb push_back
-typedef long long ll;
-typedef long double ld;
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+
+const int MOD = 1e9 + 7;
 const ll INF = ll(1e18);
 ll T, N, M;
 
