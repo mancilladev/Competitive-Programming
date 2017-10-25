c4007b42866c5ccdcfbc67578a373d8cf6fd22f9
Diego Said Anaya Mancilla <redcpp@gmail.com>
2017-10-24 22:44:11 -0500

    Got tired of writing cpp extension

--- templates/copy_template.sh
+++ templates/copy_template.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-cp ~/cp/templates/cpp_template.cpp "${PWD}/${1}"
+cp ~/cp/templates/cpp_template.cpp "${PWD}/${1}.cpp"
 
 if [[ -n "${@:2}" && "${2}" == "-s" ]]; then
-    subl "${PWD}/${1}"
+    subl "${PWD}/${1}.cpp"
 else
-    vim "${PWD}/${1}"
+    vim "${PWD}/${1}.cpp"
 fi
