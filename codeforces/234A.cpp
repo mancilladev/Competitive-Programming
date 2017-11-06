3ba104b485de86710a9f48e960e7d59a9d991177
Diego Said Anaya Mancilla <redcpp@gmail.com>
2017-11-05 19:59:18 -0600

    Solve some ladder problems

---
+++ codeforces/234A.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> pii;
+template <class T> using min_queue = priority_queue<T, vector<T>, greater<T>>;
+template <class T> using Tree = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+const int INF = (int)1e9 + 7;
+const long long LLINF = (ll)4e18 + 7;
+const double pi = acos(-1.0);
+
+#define deb(x) cerr << #x << " = " << x << endl
+#define sz(a) static_cast<int>((a).size())
+#define all(a) (a).begin(), (a).end()
+#define sq(x) (x) * (x)
+#define eb emplace_back
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+#define endl '\n'
+
+#define UNIQUE(a) sort(all(a)), (a).erase(unique(all(a)), (a).end())
+#define FOR(i, a, b) for (int i(a), b_(b); i < b_; ++i)
+#define REP(i, n) FOR (i, 0, n)
+#define FORD(i, a, b) for (int i(a), b_(b); i >= b_; --i)
+
+const int N = 1e5+7;
+int n;
+
+int main(void) {
+    freopen("input.txt", "r", stdin);
+#ifndef LOCAL
+    freopen("output.txt", "w", stdout);
+#endif
+    ios_base::sync_with_stdio(0), cin.tie(nullptr);
+    cin >> n;
+    string st; cin >> st;
+    int m = n/2;
+    REP(i, m) {
+        int j = i+m;
+        if (st[j] == 'L' && st[i] == 'R') cout << j+1 << ' ' << i+1 << endl;
+        else cout << i+1 << ' ' << j+1 << endl;
+    }
+    return 0;
+}
---
+++ codeforces/371C.cpp
@@ -0,0 +1,106 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> pii;
+template <class T> using min_queue = priority_queue<T, vector<T>, greater<T>>;
+template <class T> using Tree = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+// PAIR
+template < typename F, typename S >
+ostream& operator << ( ostream& os, const pair< F, S > & p ) {
+    return os << "(" << p.first << ", " << p.second << ")";
+}
+
+// VECTOR
+template < typename T >
+ostream &operator << ( ostream & os, const vector< T > &v ) {
+    os << "{";
+    typename vector< T > :: const_iterator it;
+    for( it = v.begin(); it != v.end(); it++ ) {
+        if( it != v.begin() ) os << ", ";
+        os << *it;
+    }
+    return os << "}";
+}
+
+// SET
+template < typename T >
+ostream &operator << ( ostream & os, const set< T > &v ) {
+    os << "[";
+    typename set< T > :: const_iterator it;
+    for ( it = v.begin(); it != v.end(); it++ ) {
+        if( it != v.begin() ) os << ", ";
+        os << *it;
+    }
+    return os << "]";
+}
+
+// MAP
+template < typename F, typename S >
+ostream &operator << ( ostream & os, const map< F, S > &v ) {
+    os << "[";
+    typename map< F , S >::const_iterator it;
+    for( it = v.begin(); it != v.end(); it++ ) {
+        if( it != v.begin() ) os << ", ";
+        os << it -> first << " = " << it -> second ;
+    }
+    return os << "]";
+}
+
+const int INF = (int)1e9 + 7;
+const long long LLINF = (ll)4e18 + 7;
+const double pi = acos(-1.0);
+
+#define deb(x) cerr << #x << " = " << x << endl
+#define sz(a) static_cast<int>((a).size())
+#define all(a) (a).begin(), (a).end()
+#define sq(x) (x) * (x)
+#define eb emplace_back
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+#define endl '\n'
+
+#define UNIQUE(a) sort(all(a)), (a).erase(unique(all(a)), (a).end())
+#define FOR(i, a, b) for (int i(a), b_(b); i < b_; ++i)
+#define REP(i, n) FOR (i, 0, n)
+#define FORD(i, a, b) for (int i(a), b_(b); i >= b_; --i)
+
+const ll N = (ll)1e12;
+ll r;
+string st;
+vector<ll> n(3), p(3), c(3);
+
+bool f(ll ham) {
+    ll cost = 0;
+    REP(i, 3) cost += max((ll)0, c[i] * ham - n[i]) * p[i];
+    return cost <= r;
+}
+
+int main(void) {
+    ios_base::sync_with_stdio(0), cin.tie(nullptr);
+    cin >> st;
+    REP(i, 3) cin >> n[i];
+    REP(i, 3) cin >> p[i];
+    cin >> r;
+
+    for (auto ch : st) {
+        if (ch == 'B') c[0]++;
+        else if (ch == 'S') c[1]++;
+        else c[2]++;
+    }
+
+    ll res = 0;
+    for (ll ham = N; ham >= 1; ham /= 2) {
+        while (f(res+ham)) res += ham;
+    }
+    cout << res << endl;
+    return 0;
+}
