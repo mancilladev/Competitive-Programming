3c8e24cc9ecf71241074758b10b7c8b5b39c4264
Diego Said Anaya Mancilla <redcpp@gmail.com>
2018-02-06 15:41:17 -0600

    Change cppt to use nvim instead of vim

--- templates/copy_template.sh
+++ templates/copy_template.sh
@@ -2,7 +2,7 @@
 cp ~/cp/templates/cpp_template.cpp "${PWD}/${1}.cpp"
 
 if [[ -n "${@:2}" && "${2}" == "-v" ]]; then
-    vim "${PWD}/${1}.cpp"
+    nvim "${PWD}/${1}.cpp"
 else
     subl "${PWD}/${1}.cpp"
 fi
